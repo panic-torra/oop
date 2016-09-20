ada19f6b56681ec54ff5db0a8801c8f4f35bc98c
panic-torra <vika15pro@yandex.ru>
2016-09-21 01:10:32 +0300

    main разбита на более мелкие функции

--- lab_1/replace/replace/replace.cpp
+++ lab_1/replace/replace/replace.cpp
@@ -3,44 +3,89 @@
 #include <fstream>
 #include <string>
 
+#define MAX_NUM_OF_ARG 5
+
 using namespace std;
 
 void CheckArgCount(int);
-void CheckInputFile(ifstream);
-void CheckOutputFile(ifstream);
+void CheckInputFile(ifstream &, string);
+void CheckOutputFile(ofstream &, string);
 void CheckSearchStr(string);
-void ReplaceStringToStringInFile(ifstream, string, string);
+void CheckWritingData(ofstream &);
+void ReplaceStringToStringInFile(ifstream &, ofstream &, char * []);
 
 int main(int argc, char * argv[])
 {
 	CheckArgCount(argc);
 
 	ifstream input(argv[1]);
-
-	if (!input.is_open())
-	{
-		cout << "Failed to open " << argv[1] << " for reading\n";
-		return 1;
-	}
+	CheckInputFile(input, argv[1]);
 
 	ofstream output(argv[2]);
+	CheckOutputFile(output, argv[2]);
+    
+	CheckSearchStr(argv[3]);
+
+	ReplaceStringToStringInFile(input, output, argv);
+
+	CheckWritingData(output);
+
+	return 0;
+}
+
+void CheckArgCount(int argc)
+{
+	if (argc != MAX_NUM_OF_ARG)
+	{
+		cout << "Invalid arguments count\n"
+			 << "Usage: replace.exe <input file> <output file> <search string> <replace string>\n";
+		exit(1);
+	}
+}
+
+void CheckInputFile(ifstream & input, string fileName)
+{
+	if (!input.is_open())
+	{
+		cout << "Failed to open " << fileName << " for reading\n";
+		exit(1);
+	}
+}
+
+void CheckOutputFile(ofstream & output, string fileName)
+{
 	if (!output.is_open())
 	{
-		cout << "Failed to open " << argv[2] << " for writing\n";
-		return 1;
+		cout << "Failed to open " << fileName << " for writing\n";
+		exit(1);
 	}
+}
 
-	const string searchString  = argv[3];
-
-	if (searchString.length() == 0)
+void CheckSearchStr(string searchStr)
+{
+	if (searchStr == "")
 	{
 		cout << "Failed to search empty string\n";
-		return 1;
+		exit(1);
 	}
+}
 
+void CheckWritingData(ofstream & output)
+{
+	if (!output.flush())
+	{
+		cout << "Failed to save data on disk\n";
+		exit(1);
+	}
+}
+
+void ReplaceStringToStringInFile(ifstream & input, ofstream & output, char * argv[])
+{
+	const string searchString  = argv[3];
 	const string replaceString = argv[4];
 	const int replaceStringLen = replaceString.length();
-	const int searchStringLen = searchString.length();
+	const int searchStringLen  = searchString.length();
+	
 	string currString;
 
 	while (getline(input, currString))
@@ -58,23 +103,5 @@ int main(int argc, char * argv[])
 
 		}
 		output << currString << "\n";
-	}
-
-	if (!output.flush())
-	{
-	    cout << "Failed to save data on disk\n";
-	    return 1;
-	}
-
-	return 0;
-}
-
-void CheckArgCount(int argc)
-{
-	if (argc != 5)
-	{
-		cout << "Invalid arguments count\n"
-			<< "Usage: replace.exe <input file> <output file> <search string> <replace string>\n";
-		exit(1);
 	}
 }

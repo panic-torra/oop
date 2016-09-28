9ae42665818ef1cdf24282ded52964860cc74167
panic-torra <vika15pro@yandex.ru>
2016-09-28 12:14:07 +0300

    Основа функции invert

--- lab_1/invert/invert/invert.cpp
+++ lab_1/invert/invert/invert.cpp
@@ -8,8 +8,14 @@
 
 using namespace std;
 
+typedef double Matrix[NUM_OF_ROWS_IN_MATRIX][NUM_OF_COLUMNS_IN_MATRIX];
+
 bool CheckArgCount(int);
 bool CheckInputFile(ifstream &);
+bool TryToGetInvertMatrix(ifstream &);
+void ReadMatrixFromFile(ifstream &, Matrix &);
+double GetDeterminant(const Matrix &);
+void PrintInversMatrix(const Matrix &, const double &);
 
 int main(int argc, char * argv[])
 {
@@ -26,9 +32,7 @@ int main(int argc, char * argv[])
 		return 1;
 	}
 
-	PrintInvertMatrix
-
-	return 0;
+	return TryToGetInvertMatrix(input);
 }
 
 bool CheckArgCount(int argc)
@@ -39,4 +43,46 @@ bool CheckArgCount(int argc)
 bool CheckInputFile(ifstream & input)
 {
 	return (!input.is_open());
+}
+
+bool TryToGetInvertMatrix(ifstream & matrixFile)
+{
+	Matrix srcMatrix;
+	ReadMatrixFromFile(matrixFile, srcMatrix);
+
+	double determinant = GetDeterminant(srcMatrix);
+	if (determinant == 0)
+	{
+		cout << "The determinant of the matrix is 0, so the inverse matrix does not exist." << endl;
+		return 1;
+	}
+
+	PrintInversMatrix(srcMatrix, determinant);
+	return 0;
+}
+
+void ReadMatrixFromFile(ifstream & matrixFile, Matrix & matrix)
+{
+	for (int i = 0; i < NUM_OF_ROWS_IN_MATRIX; ++i)
+	{
+		for (int j = 0; j < NUM_OF_ROWS_IN_MATRIX; ++j)
+		{
+			matrixFile >> matrix[i][j];
+		}
+	}
+}
+
+double GetDeterminant(const Matrix & matrix)
+{
+	return matrix[0][0] * matrix[1][1] * matrix[2][2]
+		 + matrix[0][1] * matrix[1][2] * matrix[2][0]
+		 + matrix[0][2] * matrix[1][0] * matrix[2][1]
+		 - matrix[0][2] * matrix[1][1] * matrix[2][0]
+		 - matrix[0][0] * matrix[1][2] * matrix[2][1]
+		 - matrix[0][1] * matrix[1][0] * matrix[2][2];
+}
+
+void PrintInversMatrix(const Matrix & srcMatrix, const double & determinant)
+{
+
 }

1cffc2ba1f3f64bb9f407eca1d08e0350330a3bf
panic-torra <vika15pro@yandex.ru>
2016-10-02 16:16:57 +0300

    Добавлены дополнительные тесты

--- lab_1/invert/invert/test.bat
+++ lab_1/invert/invert/test.bat
@@ -1,25 +1,36 @@
 set PROGRAM="%~1"
 
-rem check empty file
+%PROGRAM% input\wrong-argv.txt "must be error" > %TEMP%\wrong-argv.txt
+if NOT ERRORLEVEL 1 goto err
+fc.exe %TEMP%\wrong-argv.txt output\wrong-argv.txt
+if ERRORLEVEL 1 goto err
+
 %PROGRAM% input\empty.txt > %TEMP%\empty.txt
-if NOT ERRORLEVEL 1 goto err         
-
-rem check matrix with determinant = 0
-%PROGRAM% input\zero.txt > %TEMP%\zero.txt 
-if ERRORLEVEL 1 goto err
-fc.exe %TEMP%\zero.txt output\zero.txt
+if NOT ERRORLEVEL 1 goto err
+fc.exe %TEMP%\empty.txt output\empty.txt
 if ERRORLEVEL 1 goto err
 
-rem check matrix with determinant close to 0
-%PROGRAM% input\small.txt > %TEMP%\small.txt 
-if ERRORLEVEL 1 goto err
-fc.exe %TEMP%\small.txt output\small.txt
+%PROGRAM% input\non-existing.txt > %TEMP%\non-existing.txt
+IF NOT ERRORLEVEL 1 goto err
+fc.exe %TEMP%\non-existing.txt output\expected-output-when-input-file-is-missing.txt
 if ERRORLEVEL 1 goto err
 
 rem check normal matrix
-%PROGRAM% input.txt > %TEMP%\output.txt
+%PROGRAM% input\input.txt > %TEMP%\output.txt
 if ERRORLEVEL 1 goto err
 fc.exe %TEMP%\output.txt output\output.txt
+if ERRORLEVEL 1 goto err
+
+rem check normal matrix
+%PROGRAM% input\input2.txt > %TEMP%\output2.txt
+if ERRORLEVEL 1 goto err
+fc.exe %TEMP%\output2.txt output\output2.txt
+if ERRORLEVEL 1 goto err
+
+rem check matrix with determinant = 0
+%PROGRAM% input\zero.txt > %TEMP%\zero.txt 
+if NOT ERRORLEVEL 1 goto err
+fc.exe %TEMP%\zero.txt output\zero.txt
 if ERRORLEVEL 1 goto err
 
 echo Program testing succeeded

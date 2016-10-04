f1ac13b35dd51f7c3dbcce7d11ebcbcaf39ad148
panic-torra <vika15pro@yandex.ru>
2016-10-04 23:45:23 +0300

    Правки по константам, разделению объявления функции и реализации

--- lab_1/invert/invert/invert.cpp
+++ lab_1/invert/invert/invert.cpp
@@ -2,26 +2,141 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 #include <iomanip>
 
-#define MAX_NUM_OF_ARG 2
-#define NUM_OF_ROWS_IN_MATRIX 3
-#define NUM_OF_COLUMNS_IN_MATRIX 3
-#define EPS 0.0001
+const int MAX_NUM_OF_ARG = 2;
+const size_t NUM_OF_ROWS_IN_MATRIX = 3;
+const size_t NUM_OF_COLUMNS_IN_MATRIX = 3;
+const double EPS = 0.0001;
 
 using namespace std;
 
 typedef double Matrix[NUM_OF_ROWS_IN_MATRIX][NUM_OF_COLUMNS_IN_MATRIX];
 
-bool CheckArgCount(const short&);
-bool CheckInputFile(const ifstream&);
-bool IsFileEmpty(ifstream&);
-int TryToGetInvertMatrix(ifstream&);
-bool TryToReadMatrixFromFile(ifstream&, Matrix&);
-double GetDeterminant(const Matrix&);
-double GetMinor2x2(const Matrix&, const short&, const short&);
-void MakeInverseMatrix(const Matrix&, Matrix&, const double&);
-void PrintMatrix(const Matrix&);
+bool CheckArgCount(const int argc)
+{
+	return (argc != MAX_NUM_OF_ARG);
+}
+
+bool CheckInputFile(const ifstream& input)
+{
+	return (!input.is_open());
+}
+
+bool IsFileEmpty(ifstream& file)
+{
+	return file.peek() == ifstream::traits_type::eof();
+}
+
+bool TryToReadMatrixFromFile(ifstream& matrixFile, Matrix& matrix)
+{
+	for (size_t i = 0; i < NUM_OF_ROWS_IN_MATRIX; ++i)
+	{
+		for (size_t j = 0; j < NUM_OF_COLUMNS_IN_MATRIX; ++j)
+		{
+			if (!matrixFile.eof())
+			{
+				matrixFile >> matrix[i][j];
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+
+	return true;
+}
+
+double GetMinor2x2(const Matrix& matrix, const size_t row, const size_t col)
+{
+	vector <double> minorElems;
+	size_t currElem = 0;
+
+	for (size_t i = 0; i < NUM_OF_ROWS_IN_MATRIX; ++i)
+	{
+		if (i == row)
+		{
+			continue;
+		}
+		for (size_t j = 0; j < NUM_OF_COLUMNS_IN_MATRIX; ++j)
+		{
+			if (j == col)
+			{
+				continue;
+			}
+			minorElems.push_back(matrix[i][j]);
+			++currElem;
+		}
+	}
+
+	return (minorElems[0] * minorElems[3]) - (minorElems[1] * minorElems[2]);
+}
+
+double GetDeterminant(const Matrix& matrix)
+{
+	double determinant = 0;
+	const size_t i = 0;
+	short sign = 1;
+
+	for (size_t j = 0; j < NUM_OF_COLUMNS_IN_MATRIX; ++j)
+	{
+		determinant += sign * matrix[i][j] * GetMinor2x2(matrix, i, j);
+		sign *= -1;
+	}
+
+	return determinant;
+}
+
+void MakeInverseMatrix(const Matrix& srcMatrix, Matrix& dstMatrix, const double determinant)
+{
+	short sign = 1;
+	for (size_t i = 0; i < NUM_OF_ROWS_IN_MATRIX; ++i)
+	{
+		for (size_t j = 0; j < NUM_OF_COLUMNS_IN_MATRIX; ++j)
+		{
+			dstMatrix[j][i] = sign * (1 / determinant) * GetMinor2x2(srcMatrix, i, j);
+			sign *= -1;
+		}
+	}
+}
+
+void PrintMatrix(const Matrix& matrix)
+{
+	for (size_t i = 0; i < NUM_OF_ROWS_IN_MATRIX; ++i)
+	{
+		for (size_t j = 0; j < NUM_OF_COLUMNS_IN_MATRIX; ++j)
+		{
+			cout << fixed << setprecision(3) << matrix[i][j] << ' ';
+		}
+		cout << endl;
+	}
+}
+
+
+bool TryToGetInvertMatrix(ifstream& matrixFile)
+{
+	Matrix srcMatrix;
+	if (!TryToReadMatrixFromFile(matrixFile, srcMatrix))
+	{
+		cout << "There isn`t matrix 3x3 in file." << endl;
+		return true;
+	};
+
+	double determinant = GetDeterminant(srcMatrix);
+	if (fabs(determinant - 0) < EPS)
+	{
+		cout << "The determinant of the matrix is 0, so the inverse matrix does not exist." << endl;
+		return true;
+	}
+
+	Matrix dstMatrix;
+	MakeInverseMatrix(srcMatrix, dstMatrix, determinant);
+	PrintMatrix(dstMatrix);
+
+	return false;
+}
 
 int main(int argc, char * argv[])
 {
@@ -44,127 +159,4 @@ int main(int argc, char * argv[])
     }
 
     return TryToGetInvertMatrix(input);
-}
-
-int TryToGetInvertMatrix(ifstream& matrixFile)
-{
-    Matrix srcMatrix;
-    Matrix dstMatrix;
-    if (!TryToReadMatrixFromFile(matrixFile, srcMatrix))
-    {
-        cout << "There isn`t matrix 3x3 in file." << endl;
-        return 1;
-    };
-
-    double determinant = GetDeterminant(srcMatrix);
-    if (fabs(determinant - 0) < EPS)
-    {
-        cout << "The determinant of the matrix is 0, so the inverse matrix does not exist." << endl;
-        return 1;
-    }
-
-    MakeInverseMatrix(srcMatrix, dstMatrix, determinant);
-    PrintMatrix(dstMatrix);
-
-    return 0;
-}
-
-bool TryToReadMatrixFromFile(ifstream& matrixFile, Matrix& matrix)
-{
-    for (short i = 0; i < NUM_OF_ROWS_IN_MATRIX; ++i)
-    {
-        for (short j = 0; j < NUM_OF_COLUMNS_IN_MATRIX; ++j)
-        {
-            if (!matrixFile.eof())
-            {
-                matrixFile >> matrix[i][j];
-            }
-            else
-            {
-                return false;
-            }
-        }
-    }
-
-    return true;
-}
-
-double GetDeterminant(const Matrix& matrix)
-{
-    double determinant = 0;
-    const short i = 0;
-    short sign = 1;
-
-    for (short j = 0; j < NUM_OF_COLUMNS_IN_MATRIX; ++j)
-    {
-        determinant += sign * matrix[i][j] * GetMinor2x2(matrix, i, j);
-        sign *= -1;
-    }
-
-    return determinant;
-}
-
-void MakeInverseMatrix(const Matrix& srcMatrix, Matrix& dstMatrix, const double& determinant)
-{
-    short sign = 1;
-    for (short i = 0; i < NUM_OF_ROWS_IN_MATRIX; ++i)
-    {
-        for (short j = 0; j < NUM_OF_COLUMNS_IN_MATRIX; ++j)
-        {
-            dstMatrix[j][i] = sign * (1 / determinant) * GetMinor2x2(srcMatrix, i, j);
-            sign *= -1;
-        }
-    }
-}
-
-double GetMinor2x2(const Matrix& matrix, const short& row, const short& col)
-{
-    double minorElems[4];
-    short currElem = 0;
-
-    for (short i = 0; i < NUM_OF_ROWS_IN_MATRIX; ++i)
-    {
-		if (i == row)
-		{
-			continue;
-		}
-        for (short j = 0; j < NUM_OF_COLUMNS_IN_MATRIX; ++j)
-        {
-			if (j == col)
-			{
-				continue;
-			}
-            minorElems[currElem] = matrix[i][j];
-		    ++currElem;
-        }
-    }
-
-    return (minorElems[0] * minorElems[3]) - (minorElems[1] * minorElems[2]);
-}
-
-void PrintMatrix(const Matrix& matrix)
-{
-    for (short i = 0; i < NUM_OF_ROWS_IN_MATRIX; ++i)
-    {
-        for (short j = 0; j < NUM_OF_COLUMNS_IN_MATRIX; ++j)
-        {
-            cout << fixed << setprecision(3) << matrix[i][j] << ' ';
-        }
-        cout << endl;
-    }
-}
-
-bool CheckArgCount(const short& argc)
-{
-    return (argc != MAX_NUM_OF_ARG);
-}
-
-bool CheckInputFile(const ifstream& input)
-{
-    return (!input.is_open());
-}
-
-bool IsFileEmpty(ifstream& file)
-{
-    return file.peek() == ifstream::traits_type::eof();
 }

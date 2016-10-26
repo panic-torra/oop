562a30449206072c6ed80f671cee05f5e8ff3152
panic-torra <vika15pro@yandex.ru>
2016-10-26 10:48:57 +0300

    Добавлены проверки на выход за границы интервала в самих фунциях

--- lab2/primeNumbers/primeNums.cpp
+++ lab2/primeNumbers/primeNums.cpp
@@ -5,35 +5,43 @@
 std::vector<bool> InitVectorOfPrimes(unsigned int upperBound)
 {
 	std::vector<bool> vector(upperBound + 1, true);
-
-	vector[0] = vector[1] = false;
-	for (size_t i = 2; i * i < upperBound; ++i)
+	if (upperBound >= 2 && upperBound < 100000001)
 	{
-		if (vector[i])
+		vector[0] = vector[1] = false;
+		for (size_t i = 2; i * i <= upperBound; ++i)
 		{
-			for (size_t j = i * i; j <= upperBound; j += i)
+			if (vector[i])
 			{
-				vector[j] = false;
+				for (size_t j = i * i; j <= upperBound; j += i)
+				{
+					vector[j] = false;
+				}
 			}
 		}
 	}
-
-	return { vector.begin(), vector.end() };
+	else
+	{
+		vector = {};
+	}
+	return vector;
 }
 
 std::set<int> GeneratePrimeNumbersSet(unsigned int upperBound)
 {
-	std::vector<bool> tmpSet = InitVectorOfPrimes(upperBound + 1);
+	std::vector<bool> tmpSet = InitVectorOfPrimes(upperBound);
 
 	std::set<int> result;
-	result.insert(2);
-	for (size_t i = 3; i <= upperBound; i += 2)
+	if (upperBound >= 2 && upperBound < 100000001)
 	{
-		if (tmpSet[i])
+		result.insert(2);
+		for (size_t i = 3; i <= upperBound; i += 2)
 		{
-			result.insert(i);
+			if (tmpSet[i])
+			{
+				result.insert(i);
+			}
 		}
 	}
 
-	return { result.begin(), result.end() };
+	return result;
 }
--- lab2/primeNumbers/primeNums.h
+++ lab2/primeNumbers/primeNums.h
@@ -3,5 +3,5 @@
 #include <vector>
 #include <set>
 
-std::vector<bool> InitVectorOfPrimes(unsigned int upperBound);
 std::set<int> GeneratePrimeNumbersSet(unsigned int upperBound);
+std::vector<bool> InitVectorOfPrimes(unsigned int upperBound);

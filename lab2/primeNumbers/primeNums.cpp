de2495fd132ff57118727611ff946311b85b3814
panic-torra <vika15pro@yandex.ru>
2016-11-02 10:16:27 +0300

    vector -> sieve

--- lab2/primeNumbers/primeNums.cpp
+++ lab2/primeNumbers/primeNums.cpp
@@ -7,26 +7,26 @@ const int FIRST_PRIME = 2;
 
 std::vector<bool> InitVectorOfPrimes(unsigned int upperBound)
 {
-	std::vector<bool> vector(upperBound + 1, true);
+	std::vector<bool> sieve(upperBound + 1, true);
 	if (upperBound >= FIRST_PRIME && upperBound < MAX_VALUE + 1)
 	{
-		vector[0] = vector[1] = false;
+		sieve[0] = sieve[1] = false;
 		for (unsigned int i = FIRST_PRIME; i * i <= upperBound; ++i)
 		{
-			if (vector[i])
+			if (sieve[i])
 			{
 				for (unsigned int j = i * i; j <= upperBound; j += i)
 				{
-					vector[j] = false;
+					sieve[j] = false;
 				}
 			}
 		}
 	}
 	else
 	{
-		vector = {};
+		sieve = {};
 	}
-	return vector;
+	return sieve;
 }
 
 std::set<int> GeneratePrimeNumbersSet(unsigned int upperBound)

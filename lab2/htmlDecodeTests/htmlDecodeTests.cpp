b04fb4d117a851bae906c74feb28d6c413ed5271
panic-torra <vika15pro@yandex.ru>
2016-11-09 09:11:35 +0300

    BOOST_CHECK заменены на BOOST_CHECK_EQUAL для удобства проверки

--- lab2/htmlDecodeTests/htmlDecodeTests.cpp
+++ lab2/htmlDecodeTests/htmlDecodeTests.cpp
@@ -5,31 +5,31 @@ using namespace std;
 
 BOOST_AUTO_TEST_SUITE(HtmlDecode_function)
 
-	BOOST_AUTO_TEST_CASE(doesnt_spoil_string)
+	BOOST_AUTO_TEST_CASE(does_not_change_string_without_html_entity)
 	{
 		const string expectedResult = "The quick brown fox jumps over the lazy dog";
 		string input = "The quick brown fox jumps over the lazy dog";
 		auto result = HtmlDecode(input);
 
-		BOOST_CHECK(result == expectedResult);
+		BOOST_CHECK_EQUAL(result, expectedResult);
 	}
 
-	BOOST_AUTO_TEST_CASE(replace_required_substrings_once)
+	BOOST_AUTO_TEST_CASE(replaces_each_html_entity_with_corresponding_character_once)
 	{
 		const string expectedResult = "Cat <says> \"Meow. M&M's";
 		string input = "Cat &lt;says&gt; &quot;Meow. M&amp;M&apos;s";
 		auto result = HtmlDecode(input);
 
-		BOOST_CHECK(result == expectedResult);
+		BOOST_CHECK_EQUAL(result, expectedResult);
 	}
 
-	BOOST_AUTO_TEST_CASE(replace_required_repeated_substrings)
+	BOOST_AUTO_TEST_CASE(replaces_each_html_entity_with_corresponding_character)
 	{
-		const string expectedResult = "Cat <says> \"Meow\". M&M's\\\ Cat <says> \"Meow\".";
-		string input = "Cat &lt;says&gt; &quot;Meow&quot;. M&amp;M&apos;s\\\ Cat &lt;says&gt; &quot;Meow&quot;.";
+		const string expectedResult = "Cat <says> \"Meow\". M&M's. Cat <says> \"Meow\".";
+		string input = "Cat &lt;says&gt; &quot;Meow&quot;. M&amp;M&apos;s. Cat &lt;says&gt; &quot;Meow&quot;.";
 		auto result = HtmlDecode(input);
 
-		BOOST_CHECK(result == expectedResult);
+		BOOST_CHECK_EQUAL(result, expectedResult);
 	}
 
 BOOST_AUTO_TEST_SUITE_END()

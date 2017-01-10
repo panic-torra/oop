2b79084d0e7a5db77d9dee141b194996026898b3
panic-torra <vika15pro@yandex.ru>
2017-01-10 03:02:12 +0300

    Добавлен метод добавления элемента в list

--- lab6/StringList/StringList/CStringList.cpp
+++ lab6/StringList/StringList/CStringList.cpp
@@ -7,3 +7,45 @@ CStringList::CNode::CNode(const std::string & data, CNode * prev, std::unique_pt
 	, next(std::move(next))
 {
 }
+
+size_t CStringList::size() const
+{
+	return m_size;
+}
+
+bool CStringList::empty() const
+{
+	return (m_size == 0);
+}
+
+void CStringList::append(const std::string & data)
+{
+	auto newNode = std::make_unique<CNode>(data, m_lastNode, nullptr);
+	auto * newLastNode = newNode.get();
+	if (m_lastNode)
+	{
+		m_lastNode->next = move(newNode);
+	}
+	else
+	{
+		m_firstNode = move(newNode);
+	}
+	m_lastNode = newLastNode;
+	++m_size;
+}
+
+void CStringList::append(std::string && data)
+{
+	auto newNode = std::make_unique<CNode>(std::move(data), m_lastNode, nullptr);
+	auto * newLastNode = newNode.get();
+	if (m_lastNode)
+	{
+		m_lastNode->next = move(newNode);
+	}
+	else
+	{
+		m_firstNode = move(newNode);
+	}
+	m_lastNode = newLastNode;
+	++m_size;
+}
--- lab6/StringList/StringListTests/StringListTests.cpp
+++ lab6/StringList/StringListTests/StringListTests.cpp
@@ -14,5 +14,15 @@ BOOST_FIXTURE_TEST_SUITE(String_list, EmptyStringList)
 	{
 		BOOST_CHECK(list.empty());
 	}
+	BOOST_AUTO_TEST_SUITE(after_appending_element)
+		BOOST_AUTO_TEST_CASE(increase_size)
+		{
+			list.append("hello");
+			BOOST_CHECK_EQUAL(list.size(), 1);
+			list.append("world");
+			BOOST_CHECK_EQUAL(list.size(), 2);
+		}
+
+	BOOST_AUTO_TEST_SUITE_END()
 
 BOOST_AUTO_TEST_SUITE_END()
